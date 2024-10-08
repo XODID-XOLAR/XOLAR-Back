566fc153cfd7061f4ecd2856cb3005a0a6303659
Lee Hannah <mohan0226@naver.com>
2024-10-08 11:54:00 +0900

    setting: 프로젝트 초기 세팅

---
+++ xolar/src/main/java/com/xodid/xolar/XolarApplication.java
@@ -0,0 +1,13 @@
+package com.xodid.xolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XolarApplication.class, args);
+	}
+
+}

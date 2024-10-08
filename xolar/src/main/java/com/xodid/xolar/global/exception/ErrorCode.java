fdf65271582f5413c0a485d582a9306a67c7f51b
Lee Hannah <mohan0226@naver.com>
2024-10-08 16:48:15 +0900

    setting: CustomException 설정

---
+++ xolar/src/main/java/com/xodid/xolar/global/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.xodid.xolar.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "ACCOUNT-001", "사용자를 찾을 수 없습니다.")
+
+    ;
+
+    private final HttpStatus httpStatus; //HttpStatus
+    private final String code; // ex) ACCOUNT-001
+    private final String message; // 설명
+}

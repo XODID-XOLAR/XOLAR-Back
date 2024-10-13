9893216bed741cd7abfd8829d1b2de427f7efd2e
Lee Hannah <mohan0226@naver.com>
2024-10-13 20:40:54 +0900

    fix: Bill, Elcetronic 엔티티에 BaseTimeEntity 상속

--- xolar/src/main/java/com/xodid/xolar/electronic/domain/Electronic.java
+++ xolar/src/main/java/com/xodid/xolar/electronic/domain/Electronic.java
@@ -1,17 +1,19 @@
 package com.xodid.xolar.electronic.domain;
 
+import com.xodid.xolar.global.BaseTimeEntity;
 import com.xodid.xolar.solarpanel.domain.SolarPanel;
-import com.xodid.xolar.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 @Entity
+@Getter
 @DynamicInsert // insert시 지정된 default값 적용
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Electronic {
+public class Electronic extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="elec_id",updatable = false)

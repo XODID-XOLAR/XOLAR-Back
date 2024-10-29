cc62d4a9018988e99ae544ab999362a0d401a901
Lee Hannah <mohan0226@naver.com>
2024-10-29 22:38:31 +0900

    setting: https://xolar.co.kr 주소로 cors 통신 허용

--- xolar/src/main/java/com/xodid/xolar/global/config/SecurityConfig.java
+++ xolar/src/main/java/com/xodid/xolar/global/config/SecurityConfig.java
@@ -22,8 +22,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://xolar.co.kr"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);

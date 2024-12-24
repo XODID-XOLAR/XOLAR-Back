05e4d7084ae1097beff99c67e8fe90b5bd51091e
Lee Hannah <mohan0226@naver.com>
2024-12-25 00:11:38 +0900

    feat: TokenProvider 생성

--- xolar/src/main/java/com/xodid/xolar/global/exception/ErrorCode.java
+++ xolar/src/main/java/com/xodid/xolar/global/exception/ErrorCode.java
@@ -10,7 +10,11 @@ public enum ErrorCode {
     PANEL_NOT_FOUND(HttpStatus.NOT_FOUND, "PANEL-001","해당 태양광 패널을 찾을 수 없습니다."),
     ELECTRONIC_NOT_FOUND(HttpStatus.NOT_FOUND, "ELEC-001","해당 태양광 패널의 전력 정보를 찾을 수 없습니다."),
     BILL_NOT_FOUNT(HttpStatus.NOT_FOUND, "BILL-001","해당 태양광 패널의 요금 정보를 찾을 수 없습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"USER-001","해당 사용자를 찾을 수 없습니다.")
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"USER-001","해당 사용자를 찾을 수 없습니다."),
+
+    // JWT 관련 에러 코드 추가
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-001", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-002", "만료된 토큰입니다.")
     ;
 
     private final HttpStatus httpStatus; //HttpStatus
